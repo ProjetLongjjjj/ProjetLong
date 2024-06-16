a8957f045b68b442ece732d7f6d163c7c31bad11
Louis <l.oger@lecolededesign.com>
2024-06-16 17:47:46 +0200

    V2

---
+++ test.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Page d'accueil</title>
+        <link rel="stylesheet" href="accueil.css" />
+    </head>     
+    <body>
+        <form action="test.php" method="post">
+            Name: <input type="text" name="name"><br>
+            <input type="submit">
+        </form>
+
+        <?php
+            echo "<script>console.log('Hey!');</script>";
+        ?>
+    </body>
+</html>
